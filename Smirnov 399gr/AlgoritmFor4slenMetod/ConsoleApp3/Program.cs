1dded774ebfe3a504e12ff1aaf0b3ac79c513dda
Coffeegreeder <Coffeegreeder@gmail.com>
2021-12-03 14:51:41 +0300

    gg

---
+++ Smirnov 399gr/AlgoritmFor4slenMetod/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int k = 0;
+            double iskLast = 4;
+            double isk = Vicislen(iskLast);
+            bool chek = true;
+            while (true)
+            {
+                if (Math.Abs(isk - iskLast) > 0.001)
+                {
+                    Console.WriteLine(isk + " " + k);
+                    iskLast = isk;
+                    isk = Vicislen(iskLast);
+                    k ++;
+                    
+                }
+                else
+                {
+                    chek = false;
+                }
+            }
+
+        }
+
+        static double Vicislen(double a)
+        {
+            double m;
+            m = Math.Sin(a) + 4;
+            return m;
+        }
+    }
+}
